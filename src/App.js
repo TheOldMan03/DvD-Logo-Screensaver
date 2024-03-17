72d76af14ea924749bedac5e5e5969844df16c5d
Joel Antony <joel.geoanto@gmail.com>
2024-03-17 16:50:17 +0530

    Box moves

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import './App.css';
-import { useState,useEffect } from 'react';
+import { useState,useEffect, useRef } from 'react';
 
 function App() {
 
   const [leftPos,setLeftPos]=useState(0);
-  const [topPos,setTopPos]=useState(10);  
+  const [topPos,setTopPos]=useState(0); 
 
+  const [x,setX]=useState(1)
+  const [y,setY]=useState(1)
+
+  const [width,setWidth]=useState(0)
+  const [height,setHeight]=useState(0)
+
+  const ref=useRef(null)
+
+  useEffect(()=>{
+    if(ref.current){
+      let h,w;
+      h=ref.current.clientHeight
+      w=ref.current.clientWidth
+
+      setHeight(h);
+      setWidth(w);
+    }
+  },[])
+  
   function move(){
 
-    let x=setInterval(()=>{
-      setLeftPos(leftPos+10)
-      setTopPos(topPos+10)
-    },1000)
-    
-    return()=>clearInterval(x)
+    if(ref.current){
+      let z=setInterval(()=>{
+        
+        if(leftPos>=width){
+          setX(-1)
+        }
+
+        else if(leftPos<=0){
+          setX(1)
+        }
+
+        setLeftPos(leftPos+x)
+
+        if(topPos>=height){
+          setY(-1)
+        }
+
+        else if(topPos<=0){
+          setY(1)
+        }
+
+        setTopPos(topPos+y)
+
+      },12)
+      
+      return()=>clearInterval(z)
+    }
   }
 
   useEffect(move,[leftPos])
 
   return (
-    <div className="App">
+    <div className="App" ref={ref}>
       <div className="dvd_logo" style={{top:`${topPos}px`,left:`${leftPos}px`}}>
       </div>
     </div>
